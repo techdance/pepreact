9455b715e773196d07192a586778c32acaa6ade5
batangaTroy <67121288+batangaTroy@users.noreply.github.com>
2020-07-13 07:38:36 -0400

    Create showProfessionalBio.js
    Class component to show Professional Bio Window.

---
+++ src/components/showProfessionalBio.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+class ShowProfessionalBio extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="col-lg-12 mb-4">
+          <div className="profesional box box-border-radius box-shadow bg-white">
+            <div className="inner-wrap">
+              <div className="box-top position-relative">
+                <h2 className="box-subhead">
+                  <span className="icon-regular icon-user-secret"></span>{" "}
+                  Professional Bio
+                </h2>
+              </div>
+              <div className="box-middle">
+                <div className="row row-custom mb-4">
+                  <div className="col-md-12">
+                    <h4>
+                      <span className="icon-regular icon-camcorder"></span>{" "}
+                      <strong>Introduction Video</strong>
+                    </h4>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="wrap-video img-resp mb-2">
+                      <img src={this.props.professor.introVideo} />
+                    </div>
+                  </div>
+                </div>
+                <div className="content-icon position-relative">
+                  <div>
+                    <span className="icon-regular icon-shield-check"></span>{" "}
+                    <strong>Discipline</strong>
+                  </div>
+                  {this.props.professor.disciplines.map((discipline, index) => (
+                    <p className="blue-color pl-4">
+                      <a key={index} href="#">
+                        {discipline}
+                      </a>
+                    </p>
+                  ))}
+                </div>
+                <div className="content-icon position-relative">
+                  <div>
+                    <span className="icon-regular icon-user-ninja"></span>{" "}
+                    <strong>Areas of Expertise</strong>
+                  </div>
+                  <p className="blue-color pl-4">
+                    {this.props.professor.areasOfExpertise.map(
+                      (expertise, index) => (
+                        <>
+                          <a key={index} href="#">
+                            {expertise}
+                          </a>
+                          <br />
+                        </>
+                      )
+                    )}
+                  </p>
+                </div>
+                <div className="content-icon plane">
+                  <p>
+                    <span className="icon-regular icon-globe-stand"></span>{" "}
+                    <strong>
+                      {this.props.professor.internationalExerience}
+                    </strong>
+                  </p>
+                </div>
+                <div className="content-icon">
+                  <div>
+                    <span className="icon-regular icon-book-user"></span>{" "}
+                    <strong>Bio</strong>
+                  </div>
+                  <div className="text pl-4">
+                    <p>{this.props.professor.bio}</p>
+                  </div>
+                </div>
+
+                <a
+                  href={this.props.professor.cvLink}
+                  className="btn btn-blue view-cv"
+                >
+                  View CV
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ShowProfessionalBio;
