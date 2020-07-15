f2fe93add7230a8a8c155c614730a73696bf08fa
batangaTroy <67121288+batangaTroy@users.noreply.github.com>
2020-07-15 07:31:22 -0400

    Create showSearchBox.js
    Class component to show search box in top of the page.

---
+++ src/components/Navigation/showSearchBox.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class ShowSearchBox extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="wrap-top-search">
+          <div className="top-search">
+            <div className="box-search">
+              <form action="">
+                <input type="text" placeholder="Search" />
+                <button type="submit">
+                  <i className="fas fa-search"></i>
+                </button>
+              </form>
+            </div>
+            <div className="box-question">
+              <a href="#">
+                <span className="icon-solid icon-input-search"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ShowSearchBox;
