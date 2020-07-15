376ee0b1329c12cf0e25228e084e562528ed0ce6
batangaTroy <67121288+batangaTroy@users.noreply.github.com>
2020-07-15 16:53:17 -0400

    Update App.js
    No Changes in set. This is to make TroyDev branch match Dev branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-import './assets/css/style-register.css';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css';	 
-import Register from './components/Register';
-import Login from './components/Login';
-import registerImg from './assets/images/logo-tower-register.png';
-
-=======
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
@@ -34,7 +18,6 @@ import "./assets/css/fontawesome-all.css";
 
 import Example05IN_00511 from "./example05IN_00511.js";
 import ExamplePage from "./components/ExamplePage.js";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -44,21 +27,13 @@ function App() {
           <Route exact path="/register">
             <Register />
           </Route>
-<<<<<<< Updated upstream
-=======
           <Route exact path="/ExamplePage">
             <ExamplePage />
           </Route>
->>>>>>> Stashed changes
           <Route path="/">
             <Login />
           </Route>
         </Switch>
-<<<<<<< Updated upstream
-        <footer class="wrap-logo-bottom">
-          <img src={registerImg} class="block" />
-        </footer>
-=======
         {/* <footer className="wrap-logo-bottom">
           <img
             src={registerImg}
@@ -66,7 +41,6 @@ function App() {
             className="block"
           />
         </footer> */}
->>>>>>> Stashed changes
       </div>
     </Router>
   );
