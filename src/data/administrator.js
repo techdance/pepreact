a9162d5cce032e3800efc4ed379764aca05b1169
towerEdTechTroy <tmcconnell@toweredtech.com>
2020-08-04 06:42:51 -0400

    Moves all data into the administrator.js data object
    Data is moved from the components into the data object

---
+++ src/data/administrator.js
@@ -0,0 +1,278 @@
+export const administrator = {
+  pageOwner: {
+    Institution: {
+      logoA2: "./images/logo-a.png",
+      logoB2: "./images/logo-b.png",
+    },
+    Person: { firstName: "Gordon", iconImage: "./images/Gordon.png" },
+  },
+
+  badges: [
+    {
+      name: "Networking",
+      icon: "fa-chart-network",
+      iconColor: "#6C38",
+    },
+  ],
+
+  alerts: [
+    {
+      type: "message",
+      image: "images/randy.png",
+      text:
+        "Randy Wolff uploaded a document in What Artificial Intelligence Can Do for Small Business project",
+    },
+
+    {
+      type: "matches",
+      image: "",
+      text:
+        "Karthic Koothrappali, Joanne Byrne and others have matched with you as suggested partners",
+    },
+    {
+      type: "message",
+      image: "images/Inez.png",
+      text:
+        "Inez Campos was added to the Ethics in Multinational Management project",
+    },
+
+    {
+      type: "bullhorn",
+      image: "",
+      text:
+        "Register today for the International Conference on Big Data in Business",
+    },
+
+    {
+      type: "message",
+      image: "images/Li.png",
+      text: "Missed call from Li Wei",
+    },
+  ],
+
+  messages: [
+    {
+      from: "Clinton Harris",
+      time: "9:08 PM",
+      subject: "Welcome to AHEA",
+      text:
+        "We’re so glad you are taking advantage of the benefits of AHEA membership. Enclosed please find…",
+    },
+
+    {
+      from: "Clinton Harris",
+      time: "4:12 PM",
+      subject: "Your syllabus is available.",
+      text:
+        "Your syllabus template for the global business course is currently attached for review.",
+    },
+
+    {
+      from: "Inez Campos",
+      time: "2:23 PM",
+      subject: "Academic Paper",
+      text:
+        "Hello Bradley. I found an academic paper that would support our collaboration – relatively new and incorporates the cloud.",
+    },
+
+    {
+      from: "Brittany Simms",
+      time: "11:46 PM",
+      subject: "Chapter 1 Questions",
+      text:
+        "Hi Dr. Dexter – Chapter 1 has me wondering about the implications of regulations globally as well.",
+    },
+
+    {
+      from: "Inez Campos",
+      time: "8:59 PM",
+      subject: "Review Questions",
+      text:
+        "Hello Bradley. Looking forward to consolidating our review questions into a central area.",
+    },
+  ],
+
+  announcements: [
+    {
+      msg:
+        "Fall Term registration is open Wednesday, April 1 through Friday, May 1, 2020.",
+      link: "#",
+    },
+
+    {
+      msg: "AHEA Conference Grant Deadline is August 28, 2020.",
+      link: "#",
+    },
+
+    {
+      msg:
+        "Help us welcome our newest CollaboratED members: Georgia International University; Ballsbridge University, Dominica; and Edinburgh Institute for Global Affairs.",
+      link: "#",
+    },
+  ],
+  programs: [
+    { name: "Budget vs. Actuals", link: "#" },
+    { name: "Faculty/Course Matrix", link: "#" },
+    { name: "Program/Space Optimization", link: "#" },
+  ],
+
+  tasksandOverrides: [
+    { name: "Adjunct Contracts Approvals", link: "#", count: "22" },
+    { name: "Faculty Evaluations", link: "#", count: "37" },
+    { name: "Expense Report Approvals", link: "#", count: "10" },
+    { name: "Approve Fall Term Schedule & Rosters", link: "#", count: "" },
+  ],
+
+  committeesandteams: [
+    { name: "Student Success Committee", link: "#" },
+    { name: "Cross-Dept Collaboration", link: "#" },
+    { name: "Curriculum Committee", link: "#" },
+    { name: "Accreditation Task Force", link: "#" },
+  ],
+
+  calendar: {
+    month: "03",
+    year: "2020",
+    colorKeys: [
+      {
+        name: "College of communications",
+        iconColor: "toltip-icon-brown",
+      },
+      {
+        name: "College of Business",
+        iconColor: "toltip-icon-blue-light",
+      },
+      {
+        name: "College of Science & Engineering",
+        iconColor: "toltip-icon-green",
+      },
+    ],
+    data: [
+      //blue-event, green-event, orange-event, purple-event
+      {
+        date: "2020-03-05",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "http://www.google.com",
+        classname: "green-dark-event",
+      },
+      {
+        date: "2020-03-10",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "green-light-event",
+      },
+      {
+        date: "2020-03-12",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "green-dark-event",
+      },
+      {
+        date: "2020-03-16",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "blue-light-event",
+      },
+      {
+        date: "2020-03-18",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "green-light-event",
+      },
+      {
+        date: "2020-03-19",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "green-dark-event",
+      },
+      {
+        date: "2020-03-24",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "green-light-event",
+      },
+      {
+        date: "2020-03-26",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "green-dark-event",
+      },
+      {
+        date: "2020-03-30",
+        badge: true,
+        title: "Human Subjects Approval",
+        description: "<b>William Stinson</b>, <i>Organizer</i>",
+        hour: "8:00am EST",
+        link: "#",
+        textlink: "View Event <i className='fas fa-angle-double-right'></i>",
+        classname: "blue-light-event",
+      },
+    ],
+  },
+  news: [
+    {
+      source: "Inside Higher Ed",
+      sourceLink: "#",
+      title: "Study of Student Learning Outcomes",
+    },
+    { source: "AHEA", sourceLink: "#", title: "2020 Study Abroad Outlook" },
+  ],
+
+  professionalEvents: [
+    {
+      eventName: "NBEA 2020 Annual Convention and Tradeshow",
+      eventLink: "#",
+      eventDateLocation: "April 7-11, 2020 Boston, MA, USA",
+    },
+    {
+      eventName: "AACSB International Conference and Annual Meeting",
+      eventLink:
+        "https://www.aacsb.edu/events/conference/2020/international-conference-and-annual-meeting",
+      eventDateLocation: "April 26-28, 2020 Denver,CO USA",
+    },
+  ],
+};
+
+export const menuItems = [
+  { name: "Home", link: "#", icon: "icon-home" },
+  { name: "Calendar", link: "#", icon: "icon-calendar" },
+  { name: "Tasks & Overrides", link: "#", icon: "icon-clipboard-list" },
+  { name: "Committees & Teams", link: "#", icon: "icon-users-class" },
+  { name: "Reports & Assessments", link: "#", icon: "icon-chart-bar" },
+  { name: "Program Management", link: "#", icon: "icon-chart-pie" },
+  { name: "My Tiles", link: "#", icon: "icon-clone" },
+  { name: "My Files", link: "#", icon: "icon-copy" },
+  { name: "Resources", link: "#", icon: "icon-th-large" },
+  { name: "Settings", link: "#", icon: "icon-settings" },
+];
