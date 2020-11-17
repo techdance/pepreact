215449ff76fc7f29e7340b414746c1ce7e4453f0
towerEdTechTroy <67971419+towerEdTechTroy@users.noreply.github.com>
2020-11-17 15:24:26 -0500

    Adds comments

--- src/components/Page/03601_editInstitutionProfilePage/editInstitutionProfilePageContainer.js
+++ src/components/Page/03601_editInstitutionProfilePage/editInstitutionProfilePageContainer.js
@@ -21,6 +21,8 @@ async function postData(url = "", data = {}) {
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
+
+  // debug response information. can get from chrome developer tools
   console.log(response);
   console.log("Status : " + response.status);
   console.log("Content Type: " + response.headers.get("Content-Type"));
